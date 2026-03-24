0c9c3633fe77e1cd28510afee2870ccc47a9a596
Alex <alexdinh1028@gmail.com>
2026-03-24 10:59:12 -0400

    Update website for v2.0.0 release

--- src/pages/api/version.json.ts
+++ src/pages/api/version.json.ts
@@ -4,7 +4,7 @@ import type { APIRoute } from 'astro'
 export const GET: APIRoute = () => {
     const data = {
         info: 'FrameOCR Latest Version',
-        version: '1.2.3'
+        version: '2.0.0'
     }
     
     return new Response(JSON.stringify(data), {
